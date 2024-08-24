2e7e163c76dc8b2873d6739646954a547d49421f
S_Minji <151079127+MinjiSeo16@users.noreply.github.com>
2024-08-24 19:04:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 308 ms, Memory: 86708 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -5,14 +5,14 @@ public class Main{
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         
-        int count = 0; //종말의 수가 포함된 숫자의 개수 
-        int number = 666; //시작할 숫자 
+        int count = 0;
+        int number = 666; 
         
         while(count < n){
             String str = Integer.toString(number);
             
             if(str.contains("666")){
-                count++;  //문자열에 666있는지 검사 
+                count++; 
             }
             if(count == n){
                 System.out.println(number);
