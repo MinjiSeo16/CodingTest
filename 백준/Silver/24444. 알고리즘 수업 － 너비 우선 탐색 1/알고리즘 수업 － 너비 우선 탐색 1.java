8d2ca0068402f4a2593a5db6fc7ab2532cf56a88
S_Minji <151079127+MinjiSeo16@users.noreply.github.com>
2024-09-12 10:37:54 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 896 ms, Memory: 90340 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N,M,R;
+    static int count = 1; 
+    static int[] order; 
+    static ArrayList<ArrayList<Integer>> list; 
+    static boolean[] visit;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+        
+        order = new int[N+1];
+        visit = new boolean[N+1];
+        list = new ArrayList<>();
+
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            
+            list.get(node1).add(node2);
+            list.get(node2).add(node1);
+        }
+        
+        for(int i=1; i<=N; i++){
+            Collections.sort(list.get(i));
+        }
+        
+        bfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            sb.append(order[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void bfs(int node){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
+        visit[node] = true;
+        order[node] = count++;
+        
+        while(!queue.isEmpty()){
+            int point = queue.poll();
+            for(int k: list.get(point)){
+                if(!visit[k]){
+                    visit[k] = true;
+                    order[k] = count++;
+                    queue.add(k);
+                }
+            }
+        }
+    }
+}
