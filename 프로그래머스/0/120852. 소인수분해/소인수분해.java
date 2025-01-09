5c78a27e13977c597651d66f495fe7240d738b53
S_Minji <151079127+MinjiSeo16@users.noreply.github.com>
2025-01-09 15:48:41 +0900

    [level 0] Title: 소인수분해, Time: 3.67 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        Set<Integer> set = new TreeSet<>();
+        
+        for(int i = 2; i <= n; i++){
+            while(n % i == 0){
+                n /= i;
+                set.add(i);
+            }
+        }
+        
+        
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
