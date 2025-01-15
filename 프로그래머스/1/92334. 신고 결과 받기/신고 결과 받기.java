48486349011c926c4bca94f01bee34dc1324844d
S_Minji <151079127+MinjiSeo16@users.noreply.github.com>
2025-01-15 11:52:16 +0900

    [level 1] Title: 신고 결과 받기, Time: 1998.98 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] count = new int[id_list.length];
-        ArrayList<String> stopList = new ArrayList<>();
-        ArrayList <ArrayList<String>> reporter = new ArrayList<>();
+        ArrayList<Integer> stopList = new ArrayList<>();
+        ArrayList <ArrayList<Integer>> reporter = new ArrayList<>();
         int[] result = new int[id_list.length];
         
         for(int i = 0; i < id_list.length; i++){
@@ -16,40 +16,29 @@ class Solution {
             String n1 = n[0];
             String n2 = n[1];
             
-            int idx = -1;
+            int idx1 = -1, idx2 = -1;
             for(int i = 0; i < id_list.length; i++){
-                if(id_list[i].equals(n2)){
-                    idx = i;
-                    break;
-                }
+                if(id_list[i].equals(n2)) idx2 = i;
+                if(id_list[i].equals(n1)) idx1 = i;
             }
             
             boolean isExist = false;
-            for(String s : reporter.get(idx)){
-                if(s.equals(n1)) isExist = true;
+            for(int number : reporter.get(idx2)){
+                if(number == idx1) isExist = true;
             }
             if(!isExist){
-                reporter.get(idx).add(n1);
-                count[idx]++;
+                reporter.get(idx2).add(idx1);
+                count[idx2]++;
             }
         }
         
         for(int i = 0; i < count.length; i++){
-            if(count[i] >= k) stopList.add(id_list[i]);
+            if(count[i] >= k) stopList.add(i);
         }
         
-        for(String str : stopList){
-            for(int i = 0; i < id_list.length; i++){
-                if(str.equals(id_list[i])){
-                    for(String reporterName : reporter.get(i)){
-                        for(int j = 0; j < id_list.length; j++){
-                            if(reporterName.equals(id_list[j])){
-                                result[j]++;
-                                break;
-                            }
-                        }
-                    }
-                }
+        for(int n : stopList){
+            for(int reporterNum : reporter.get(n)){
+                result[reporterNum]++;
             }
         }
         
