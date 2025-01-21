290448ea4887b9938538a20b4a80f31bb70aa174
S_Minji <151079127+MinjiSeo16@users.noreply.github.com>
2025-01-21 17:53:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 23176 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,41 +2,34 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    static int N,K;
+    static int[] arr;
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[100001];
         
-        System.out.println(bfs(n,k));
+        System.out.println(bfs(N));
     }
     
-    public static int bfs(int start, int target){
-        if(start == target){
-            return 0;
-        }
-        
-        boolean[] visited = new boolean[100001];
+    public static int bfs(int startX){
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
-        int time = 0;
+        queue.offer(startX);
+        arr[startX] = 0;
         
         while(!queue.isEmpty()){
-            int size = queue.size();
-            time++;
+            int position = queue.poll();
             
-            for(int i=0; i<size; i++){
-                int current = queue.poll();
-                int[] nextPositions = {current - 1, current + 1, current * 2};
-                for(int next : nextPositions){
-                    if(next == target){
-                        return time;
-                    }
-                    if(next >= 0 && next <= 100000 && !visited[next]){
-                        visited[next] = true;
-                        queue.add(next);
-                    }
+            if(position == K) return arr[position];
+            
+            int[] nextPositions = {position + 1, position - 1, position * 2};
+            for(int next : nextPositions){
+                if(next >= 0 && next <= 100000 && arr[next] == 0 && next != startX){
+                    arr[next] = arr[position] + 1;
+                    queue.offer(next);
                 }
             }
         }
