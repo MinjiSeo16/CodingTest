70e7abfc44c5fe52ce6fab5d4ce083b258dd85b6
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-09-28 11:20:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.95 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,37 +1,18 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        StringBuilder sb = new StringBuilder();
-        char[][] arr3 = new char[n][n];
-        char[][] arr4 = new char[n][n];
         
         for(int i = 0; i < n; i++) {
-            String binary3 = Integer.toBinaryString(arr1[i]);
-            String binary4 = Integer.toBinaryString(arr2[i]);
+            int map = arr1[i] | arr2[i];
             
-            while(binary3.length() < n) {
-                binary3 = "0" + binary3;
-            }
-            while(binary4.length() < n) {
-                binary4 = "0" + binary4;
-            }
+            String binary = Integer.toBinaryString(map);
             
-            arr3[i] = binary3.toCharArray();
-            arr4[i] = binary4.toCharArray();
+            binary = String.format("%" + n + "s", binary);
+            
+            binary = binary.replace('0', ' ').replace('1', '#');
+
+            answer[i] = binary;
         }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(arr3[i][j] == '0' && arr4[i][j] == '0'){
-                    sb.append(' ');
-                } else {
-                    sb.append('#');
-                }
-            }
-            answer[i] = sb.toString();
-            sb.setLength(0);
-        }
-        
         return answer;
     }
 }
