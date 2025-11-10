b488d7835090c7893a4fc2b850e95148260bc34c
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-11-10 23:04:10 +0900

    [level 3] Title: 등굣길, Time: 0.61 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,26 +2,24 @@ class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int value = 1000000007;
         int[][] dp = new int[n + 1][m + 1];
+        dp[1][1] = 1;
         
-        for(int[] p : puddles){
+        for(int[] p : puddles) {
             dp[p[1]][p[0]] = -1;
         }
         
-        dp[1][1] = 1;
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < m + 1; j++){
-                if((i == 1 && j == 1) || dp[i][j] == -1){
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
                     continue;
                 }
-                if(i > 1 && dp[i - 1][j] != -1){
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % value;
-                }
-                if(j > 1 && dp[i][j - 1] != -1){
-                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % value;
-                }
+                if(i == 1 && j == 1) continue;
+                
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % value;
             }
         }
         
-        return dp[n][m] % value;
+        return dp[n][m];
     }
 }
