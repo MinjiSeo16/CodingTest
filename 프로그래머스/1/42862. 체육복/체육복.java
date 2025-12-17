6d35240f41d2cd155cf014b2f3896e88ffec4823
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-17 14:12:54 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,25 +2,24 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int[] count = new int[n + 1];
         
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++) {
             count[i] = 1;
         }
         
-        for(int r : reserve){
+        for(int r : reserve) {
             count[r]++;
         }
         
-        for(int l : lost){
+        for(int l : lost) {
             count[l]--;
         }
         
-        for(int i = 1; i <= n; i++){
-            if(count[i] == 0){
-                if(i - 1 > 0 && count[i - 1] > 1){
+        for(int i = 1; i <= n; i++) {
+            if(count[i] == 0) {
+                if(i - 1 > 0 && count[i - 1] == 2) {
                     count[i] = 1;
                     count[i - 1] = 1;
-                }
-                else if(i + 1 <= n && count[i + 1] > 1){
+                } else if(i + 1 <= n && count[i + 1] == 2) {
                     count[i] = 1;
                     count[i + 1] = 1;
                 }
@@ -28,10 +27,8 @@ class Solution {
         }
         
         int result = 0;
-        for(int c : count){
-            if(c >= 1){
-                result++;
-            }
+        for(int i = 1; i <= n; i++) {
+            if(count[i] > 0) result++;
         }
         
         return result;
