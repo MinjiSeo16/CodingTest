d05137bcda4bad7997f52c175e5d334de744c8f0
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-17 11:21:58 +0900

    [level 3] Title: 순위, Time: 12.26 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 12.03 ms
+메모리: 91 MB, 시간: 12.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 11일 22:59:48
+2025년 12월 17일 11:21:52
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,39 +1,39 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        int[][] graph = new int[n + 1][n + 1];
-        
-        for(int[] r : results) {
-            int a = r[0];
-            int b = r[1];
-            graph[a][b] = 1;
-        }
-        
-        for(int k = 1; k <= n; k++) {
-            for(int i = 1; i <= n; i++) {
-                for(int j = 1; j <= n; j++) {
-                    if(graph[i][k] == 1 && graph[k][j] == 1) {
-                        graph[i][j] = 1;
-                    }
-                }
-            }
-        }
-        
-        int answer = 0;
-        
-        for(int i = 1; i <= n; i++) {
-            int win = 0;
-            int lose = 0;
-            
-            for(int j = 1; j <= n; j++) {
-                if(graph[i][j] == 1) win++;
-                if(graph[j][i] == 1) lose++;
-            }
-            
-            if(win + lose == n - 1) {
-                answer++;
-            }
-        }
-        
-        return answer;
+        int count = 0;
+		int[][] graph = new int[n + 1][n + 1];
+
+		for(int i = 0; i < results.length; i++) {
+			int[] ab = results[i];
+			int a = ab[0];
+			int b = ab[1];
+			graph[a][b] = 1;
+		}
+
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					if(graph[i][k] == 1 && graph[k][j] == 1) {
+						graph[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i <= n; i++) {
+			int lose = 0;
+			int win = 0;
+
+			for (int j = 1; j <= n; j++) {
+				if(graph[i][j] == 1) win++;
+				if(graph[j][i] == 1) lose++;
+			}
+
+			if(lose + win == n - 1) {
+				count++;
+			}
+		}
+
+		return count;
     }
 }
