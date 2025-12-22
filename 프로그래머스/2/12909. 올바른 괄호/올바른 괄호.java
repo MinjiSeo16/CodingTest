ff0ec4f1920c5924398f9e8058f287b0b96d2eac
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-22 13:47:54 +0900

    [level 2] Title: 올바른 괄호, Time: 18.91 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,12 +4,11 @@ class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
         
-        for(char c : s.toCharArray()){
-            if(c == '('){
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
                 stack.push(c);
-            } 
-            else{
-                if(stack.isEmpty()){
+            } else {
+                if(stack.isEmpty()) {
                     return false;
                 }
                 stack.pop();
