ad4f72809121d306426d95b9c6f7513f58accf9b
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-22 14:26:08 +0900

    [level 2] Title: 주식가격, Time: 37.66 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,27 @@
+import java.util.*;
+
 class Solution {
     
     public int[] solution(int[] prices) {
-        int[] result = new int[prices.length];
-        
-        for(int i = 0; i < prices.length; i++){
-            result[i] = countBigger(prices, i);
-        }
-        
-        return result;
-    }
-    
-    public static int countBigger(int[] prices, int index){
-        int count = 0;
-        for(int i = index + 1; i < prices.length; i++){
-            count++;
-            if(prices[i] < prices[index]){
-                break;
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+         
+        for(int i = 0; i < n; i++) {
+            // 가격이 떨어지는 순간
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
+            stack.push(i);
         }
-        return count;
+        
+        // 끝까지 가격이 떨어지지 않은 경우
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
+        }
+        
+        return answer;
     }
 }
