dc26b702c207d003abe29b162afa0818462e50cf
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-22 14:07:30 +0900

    [level 2] Title: 프로세스, Time: 2.08 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Deque<int[]> deque = new LinkedList<>(); 
+        Deque<int[]> deque = new LinkedList<>();
         int count = 0;
         
-        for(int i = 0; i < priorities.length; i++){
-            deque.add(new int[]{priorities[i], i});
+        for(int i = 0; i < priorities.length; i++) {
+            deque.offer(new int[]{priorities[i], i});
         }
         
-        while(!deque.isEmpty()){
+        while(!deque.isEmpty()) {
             int[] current = deque.pollFirst();
             
-            if(isExistBigger(current[0], deque)){
+            if(isExistBigger(current[0], deque)) {
                 deque.addLast(current);
-            } else{
+            } else {
                 count++;
-                if(current[1] == location){
+                if(location == current[1]) {
                     return count;
                 }
             }
         }
+        
         return count;
     }
     
-    public static boolean isExistBigger(int n, Deque<int[]> deque){
-        for(int[] arr : deque){
-            if(arr[0] > n){
+    private boolean isExistBigger(int n, Deque<int[]> deque) {
+        for(int[] d : deque) {
+            if(d[0] > n) {
                 return true;
             }
         }
