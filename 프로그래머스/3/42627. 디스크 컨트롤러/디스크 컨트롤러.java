b58557c0cef7872d7957cc6426f13a539d8c40ac
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-24 12:39:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.85 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
         
         PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> {
             if(a[1] != b[1]) return Integer.compare(a[1], b[1]); 
@@ -12,20 +13,20 @@ class Solution {
         int currentTime = 0;
         int total = 0;
         int index = 0;
-        int count = 0;
+        int cnt = 0;
         
-        while(count < jobs.length){
-            while(index < jobs.length && jobs[index][0] <= currentTime){
+        while(cnt < jobs.length) {
+            while(index < jobs.length && jobs[index][0] <= currentTime) {
                 q.add(jobs[index]);
                 index++;
             }
             
-            if(!q.isEmpty()){
+            if(!q.isEmpty()) {
                 int[] job = q.poll();
                 currentTime += job[1];
                 total += (currentTime - job[0]);
-                count++;
-            } else{
+                cnt++;
+            } else {
                 currentTime = jobs[index][0];
             }
         }
