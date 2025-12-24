aaeb5d5124a02931b5a1042627ba225cc0b45374
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-24 14:22:53 +0900

    [level 3] Title: 이중우선순위큐, Time: 64.79 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,40 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        Queue<Integer> q = new PriorityQueue<>();
-        Queue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> p = new PriorityQueue<>();
+        Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         int[] result = new int[2];
         
-        for(String command : operations){
-            if(command.startsWith("I")){
+        for(String command : operations) {
+            if(command.startsWith("I")) {
                 String[] str = command.split(" ");
                 int num = Integer.parseInt(str[1]);
-                q.add(num);
                 p.add(num);
+                q.add(num);
             }
             
-            if(command.equals("D -1")){
-                if(!q.isEmpty()){
-                    int min = q.poll();
-                    p.remove(min);
-                }
+            if(command.equals("D 1")) {
+                if(q.isEmpty()) continue;
+                int num = q.poll();
+                p.remove(num);
             }
             
-            if(command.equals("D 1")){
-                if(!p.isEmpty()){
-                    int max = p.poll();
-                    q.remove(max);
-                }
+            if(command.equals("D -1")) {
+                if(p.isEmpty()) continue;
+                int num = p.poll();
+                q.remove(num);
             }
         }
         
-        if(!q.isEmpty()){
-            result[0] = p.peek();
-            result[1] = q.peek();
-        }
-        else{
+        if(p.isEmpty()) {
             result[0] = 0;
             result[1] = 0;
+        } else {
+            result[0] = q.peek();
+            result[1] = p.peek();
         }
         
         return result;
