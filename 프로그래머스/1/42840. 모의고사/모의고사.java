968d5d764f5154557dfd748dc4d281f1c8a35a67
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-26 11:01:59 +0900

    [level 1] Title: 모의고사, Time: 3.29 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,29 +4,29 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; 
-        int[] score = {0, 0, 0};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] score = new int[3];
         
-        for(int i = 0; i < answers.length; i++){
+        for(int i = 0; i < answers.length; i++) {
             if(answers[i] == one[i % 5]) score[0]++;
             if(answers[i] == two[i % 8]) score[1]++;
             if(answers[i] == three[i % 10]) score[2]++;
         }
         
         int max = 0;
-        List<Integer> result = new ArrayList<Integer>();
-        for(int i = 0; i < 3; i++){
-            if(max < score[i]){
+        for(int i = 0; i < score.length; i++) {
+            if(score[i] > max) {
                 max = score[i];
             }
         }
         
-        for(int i = 0; i < 3; i++){
-            if(score[i] == max){
-                result.add(i + 1);
-            }
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < score.length; i++) {
+            if(max == score[i]) list.add(i + 1);
         }
         
-        return result.stream().mapToInt(a -> a).toArray();
+        Collections.sort(list);
+        
+        return list.stream().mapToInt(a -> a).toArray();
     }
 }
