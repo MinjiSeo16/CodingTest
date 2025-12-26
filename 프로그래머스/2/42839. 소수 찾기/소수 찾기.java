a4f56131ddc34f1c68bfc6686558722345124264
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-26 11:27:59 +0900

    [level 2] Title: 소수 찾기, Time: 25.90 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,8 +7,8 @@ class Solution {
         
         createNum("", numbers, set);
         
-        for(int num : set){
-            if(isPrime(num)){
+        for(int num : set) {
+            if(isPrime(num)) {
                 count++;
             }
         }
@@ -16,19 +16,20 @@ class Solution {
         return count;
     }
     
-    public void createNum(String prefix, String num, Set<Integer> set){
-        if(!prefix.isEmpty()){
+    private void createNum(String prefix, String num, Set<Integer> set) {
+        if(!prefix.isEmpty()) {
             set.add(Integer.parseInt(prefix));
         }
-        for(int i = 0; i < num.length(); i++){
+        
+        for(int i = 0; i < num.length(); i++) {
             createNum(prefix + num.charAt(i), num.substring(0, i) + num.substring(i + 1), set);
         }
     }
     
-    public boolean isPrime(int n){
-        if(n < 2) return false;
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0) return false;
+    private boolean isPrime(int num) {
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
         }
         return true;
     }
