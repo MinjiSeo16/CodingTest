fb0dbd61c36d0aae20f4f8476cedfc53274a5337
MinjiSeo16 <151079127+MinjiSeo16@users.noreply.github.com>
2025-12-31 12:06:12 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 456 ms, Memory: 46952 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int count = (int) Math.pow(2, N) - 1;
+		sb.append(count).append('\n');
+
+		hanoi(N, 1, 3, 2);
+        System.out.println(sb);
+	}
+
+	static void hanoi(int n, int from, int to, int via) {
+		if(n == 1) {
+			sb.append(from).append(" ").append(to).append('\n');
+			return;
+		}
+
+		hanoi(n - 1, from, via, to);
+		sb.append(from).append(" ").append(to).append('\n');
+		hanoi(n - 1, via, to, from);
+	}
+}
